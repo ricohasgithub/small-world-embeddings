29eb04ce2884e905ab7975ab1d623a89dcd4a052
Rico Zhu <ricozhu2003@gmail.com>
2024-04-15 09:53:11 -0400

    Updated dataset

--- dataset.py
+++ dataset.py
@@ -2,6 +2,8 @@
 import torch
 import torch_geometric
 
+import pickle
+
 import numpy as np
 import networkx as nx
 import matplotlib as plt
@@ -13,30 +15,34 @@ from smallworld import get_smallworld_graph
 from utils import nx_to_torch_geometric
 
 class SmallWorldDataset(Dataset):
-    def __init__(self, graphs, max_n, transform=None, pre_transform=None):
+    def __init__(self, graphs, max_n, root="./datasets", transform=None, pre_transform=None):
         super().__init__(None, transform, pre_transform)
-        self.data_list = graphs
         self.max_n = max_n
+        if graphs is not None:
+            self.data_list = graphs
+        else:
+            # Load dataset from root directory
+            pass
 
     def len(self):
-        return len(self.graphs)
+        return len(self.data_list)
 
     def get(self, idx):
-        return self.graphs[idx]
+        return self.data_list[idx]
 
 def get_sw_graph(n, k_over_2, beta, y, coloring_strategy="saturation_largest_first"):
     small_world = get_smallworld_graph(n, k_over_2, beta)
     coloring = nx.coloring.greedy_color(small_world, strategy=coloring_strategy)
     return nx_to_torch_geometric(small_world, coloring, y)
 
-def generate_sw_dataset(num_graphs, max_n, beta_threshold,
+def generate_sw_dataset(filepath, num_graphs, max_n, beta_threshold,
                         n_mean, n_variance,
                         k_over_2_mean, k_over_2_variance,
                         beta_mean, beta_variance,
                         coloring_strategy="saturation_largest_first"):
 
     graphs = []
-    for _ in range(num_graphs):
+    for i in range(num_graphs):
 
         # Sample n, k_over_2, beta
         sample_n = min(int(round(np.random.normal(n_mean, np.sqrt(n_variance)))), max_n)
@@ -45,6 +51,13 @@ def generate_sw_dataset(num_graphs, max_n, beta_threshold,
 
         # Get ground truth - set threshold on beta
         y = 1 if sample_beta >= beta_threshold else 0
-        graphs.append(get_sw_graph(sample_n, sample_k_over_2, sample_beta, y, coloring_strategy=coloring_strategy))
+        graph = get_sw_graph(sample_n, sample_k_over_2, sample_beta, y, coloring_strategy=coloring_strategy)
+
+        # Save graph to filepath
+        pickle.dump(graph, open(f"graph_{i}.pickle", "wb"))
+        graphs.append(graph)
     
     return SmallWorldDataset(graphs, max_n)
+
+if __name__ == "__main__":
+    pass
