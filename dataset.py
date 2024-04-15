57c7b8e6de6e2b068c79562596cc314988ff5b47
Rico Zhu <ricozhu2003@gmail.com>
2024-04-15 10:02:47 -0400

    Updated dataset

--- dataset.py
+++ dataset.py
@@ -1,8 +1,9 @@
 
+import json
+import pickle
+
 import torch
 import torch_geometric
-
-import pickle
 
 import numpy as np
 import networkx as nx
@@ -33,7 +34,7 @@ class SmallWorldDataset(Dataset):
 def get_sw_graph(n, k_over_2, beta, y, coloring_strategy="saturation_largest_first"):
     small_world = get_smallworld_graph(n, k_over_2, beta)
     coloring = nx.coloring.greedy_color(small_world, strategy=coloring_strategy)
-    return nx_to_torch_geometric(small_world, coloring, y)
+    return nx_to_torch_geometric(small_world, coloring, y), small_world, coloring
 
 def generate_sw_dataset(filepath, num_graphs, max_n, beta_threshold,
                         n_mean, n_variance,
@@ -45,19 +46,26 @@ def generate_sw_dataset(filepath, num_graphs, max_n, beta_threshold,
     for i in range(num_graphs):
 
         # Sample n, k_over_2, beta
-        sample_n = min(int(round(np.random.normal(n_mean, np.sqrt(n_variance)))), max_n)
-        sample_k_over_2 = int(round(np.random.normal(k_over_2_mean, np.sqrt(k_over_2_variance))))
-        sample_beta = np.random.normal(beta_mean, np.sqrt(beta_variance))
+        sample_n = max(10, min(int(round(np.random.normal(n_mean, np.sqrt(n_variance)))), max_n))
+        sample_k_over_2 = max(1, int(round(np.random.normal(k_over_2_mean, np.sqrt(k_over_2_variance)))))
+        sample_beta = max(0.025, min(np.random.normal(beta_mean, np.sqrt(beta_variance)), 0.4))
 
         # Get ground truth - set threshold on beta
         y = 1 if sample_beta >= beta_threshold else 0
-        graph = get_sw_graph(sample_n, sample_k_over_2, sample_beta, y, coloring_strategy=coloring_strategy)
+        graph, _, coloring = get_sw_graph(sample_n, sample_k_over_2, sample_beta, y, coloring_strategy=coloring_strategy)
 
         # Save graph to filepath
-        pickle.dump(graph, open(f"graph_{i}.pickle", "wb"))
+        pickle.dump(graph, open(f"{filepath}/graph_{i}.pickle", "wb"))
+        # Writing JSON data
+        with open(f"{filepath}/graph_{i}.json", "w") as coloring_file:
+            json.dump(coloring, coloring_file)
         graphs.append(graph)
+        print(f"Graph: {i} generated, n={sample_n}, k_over_2={sample_k_over_2}, beta={sample_beta}")
     
     return SmallWorldDataset(graphs, max_n)
 
 if __name__ == "__main__":
-    pass
+    generate_sw_dataset("./dataset", 100, 50, 0.25,
+                        25, 10,
+                        5, 2,
+                        0.25, 0.1)
