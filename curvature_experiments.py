9a3fdc12d4433ab52da553d287c647e88148fbc0
Rico Zhu <ricozhu2003@gmail.com>
2024-04-19 23:13:28 -0400

    Added curvature experiments

--- curvature_experiments.py
+++ curvature_experiments.py
@@ -7,4 +7,35 @@ import matplotlib.pyplot as plt
 from smallworld import get_smallworld_graph
 from GraphRicciCurvature.OllivierRicci import OllivierRicci
 
-N = np.arange(10, 1000, )
+def show_results(G):
+    # Print the first five results
+    print("Karate Club Graph, first 5 edges: ")
+    for n1,n2 in list(G.edges())[:5]:
+        print("Ollivier-Ricci curvature of edge (%s,%s) is %f" % (n1 ,n2, G[n1][n2]["ricciCurvature"]))
+
+    # Plot the histogram of Ricci curvatures
+    plt.subplot(2, 1, 1)
+    ricci_curvtures = nx.get_edge_attributes(G, "ricciCurvature").values()
+    plt.hist(ricci_curvtures,bins=20)
+    plt.xlabel('Ricci curvature')
+    plt.title("Histogram of Ricci Curvatures (Karate Club)")
+
+    # Plot the histogram of edge weights
+    plt.subplot(2, 1, 2)
+    weights = nx.get_edge_attributes(G, "weight").values()
+    plt.hist(weights,bins=20)
+    plt.xlabel('Edge weight')
+    plt.title("Histogram of Edge weights (Karate Club)")
+
+    plt.tight_layout()
+
+N = np.arange(10, 1000, 1)
+betas = np.arange(0.025, 0.5, 0.01)
+
+for n in N:
+    for beta in betas:
+        G = get_smallworld_graph(n, 2, beta)
+        orc = OllivierRicci(G, alpha=0.5, verbose="TRACE")
+        orc.compute_ricci_curvature()
+        G_orc = orc.G.copy()
+        show_results(G_orc)
